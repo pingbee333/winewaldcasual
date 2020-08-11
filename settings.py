7e22f3596341278d617d3defa88cc8c0c20ff4aa
pingbee333 <65789433+pingbee333@users.noreply.github.com>
2020-08-11 00:45:39 -0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,40 @@
+import os
+
+CURRECT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+CONFIG = {
+    "bot": {
+        "token": "NzEzNTc1ODAwOTk3MTUwNzkx.Xsimqw.dieAobR7oWSTec9As5twRpaJS8o",
+        "embed_color": 0xaeb8da,
+    },
+
+    "lang": "en",
+
+    "refresh_rate": 30,
+
+    "smart_presence": {
+        "enable": True,
+        "name": False,  # If false will use current map.
+    },
+
+    "message_ids": "{}/message_ids.txt".format(CURRECT_DIR),
+
+    "servers": {
+        "CSGO": {
+            "char_limit": 25,
+            "channel": 713504928823377960,
+            "servers": {
+                "45.235.98.240:27018": "Combat/1v1/+",
+                "45.235.98.240:27018": "Combat/1v1/+",
+            },
+        },
+        "CSGO": {
+            "char_limit": 25,
+            "channel": 713504928823377960,
+            "servers": {
+                "45.235.98.240:27018": False,
+                "45.235.98.240:27018": False,
+            },
+        },
+    },
+}
---
+++ translations.py
@@ -0,0 +1,10 @@
+TRANSLATIONS = {
+    "en": {
+        "successful": "**Mapa:** {}\n**Jugadores:** {}/{}\n**Conectarse:**\nsteam://connect/{}",
+        "fail": "Server off",
+        "fail_title": "Server offline",
+        "title": "__**{}**__",
+        "total_players": "{}/{} on our servers.",
+        "smart_presence": "{}/{} en {}",
+    },
+}
